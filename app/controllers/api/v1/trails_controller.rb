b8947a84ccdc1698999debde0660cf9c5e631a38
zanibruno <corporate.zanin@gmail.com>
2020-06-19 18:10:45 +0100

    basic crud action for trails

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -1,8 +1,42 @@
 class Api::V1::TrailsController < ApplicationController
 
+	before_action :set_location, only: [:index]
+
 	def index 
-		location - Location.all 
-		trails = location.trails
-		render json: trails
+		@trails = @location.trails
+		render json: @trails
+	end 
+
+	def show 
+		trail = Trail.find_by(id: params[id])
+		render json: trail
+	end
+
+	def create 
+		trail = @location.trail.build(trails_params)
+		if trail.save
+			render json: trail 
+		else 
+			render json: {error: "Trail not Created"}
+		end 
+	end 
+
+	def destroy 
+		trail = Trail.find_by(id: params[:id])
+		trail.destroy
+	end 
+
+	def update
+
+	end 
+
+	private
+
+	def set_location
+		@location = Location.find_by(id: params[:location_id])
+	end 
+
+	def trails_params
+		params.require(:trail).permit(:name, :length, :level, :description, :locatin_id)
 	end 
 end
