358c719548afadb3a733a89adc80cd3f43548ef6
zanibruno <corporate.zanin@gmail.com>
2020-06-19 18:10:31 +0100

    basic crud actions for location

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,7 +1,35 @@
 class Api::V1::LocationsController < ApplicationController
 
+	
+
 	def index 
-		locations = Location.all 
-		render json: locations, include: {trails: {only: [:name, :length, :level, :description, :location_id]}}
+		@locations = Location.all 
+		render json: @locations, include: {trails: {only: [:name, :length, :level, :description, :location_id]}}
 	end
+
+	def create
+		location = Location.new(location_params)
+		if location.save
+			render json: location 
+		else 
+			render json: {error: "Location no created"}
+		end
+	end 
+
+	def show
+		location = Location.find+by(id: params[:id])
+		render json: location
+	end 
+
+	def destroy 
+		location = Location.find_by(id: params[:id])
+		location.destroy
+	end 
+
+	private 
+
+
+	def location_params
+		params.require(:location).permit(:postcode, :name)
+	end 
 end
